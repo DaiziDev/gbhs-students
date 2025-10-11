be0c3f5d65e08342062fd5c28b864b8db86a201d
DaiziDev <shadowapex308@gmail.com>
2025-10-11 08:24:28 +0100

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const menu = document.querySelector("#menu");
+const toggle = document.querySelector(".fa-bars");
+const nav = document.querySelector(".small-screen")
+const darkmood = document.querySelector(".dark-mood")
+const moon = document.querySelector(".fa-moon")
+const html = document.querySelector('html')
+
+menu.addEventListener("click", function() {
+    if(toggle.classList.contains("fa-bars")){
+        toggle.classList.remove("fa-bars")
+        toggle.classList.add("fa-xmark");
+        nav.classList.remove("hidden")
+        nav.classList.add("nav-animation")
+    }else{
+        toggle.classList.remove("fa-xmark");
+        toggle.classList.add("fa-bars")
+        nav.classList.add ("hidden")
+        nav.classList.remove("nav-animation")
+    }
+})
+darkmood.addEventListener("click", function() {
+    if(moon.classList.contains("fa-moon")){
+        moon.classList.remove("fa-moon");
+        moon.classList.add("fa-sun")
+        html.classList.add("dark-mode")
+    }
+    else{
+        moon.classList.remove("fa-sun")
+        moon.classList.add("fa-moon")
+        html.classList.remove("dark-mode")
+    }
+})
+
+const apiKey = "222535634254612"
+const apiSecret = "6NwGWdKyVPzfWB2IErHmhnVjC7M"
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: {
+        'bgcolor': 'var(--bg-color)',
+      },
+      textColor: {
+        'primary': 'var(--primary)',
+      },
+    },
+  },
+}
